e094d9b6f35ee1a56e9c12f14899ea6e49ef90fc
anton.voskobovich <avoskobovitch@openwaygroup.com>
2017-04-22 22:03:25 +0300

    rollback db

--- src/main/java/ru/zapovednik/db/Tag.java
+++ src/main/java/ru/zapovednik/db/Tag.java
@@ -1,30 +1,25 @@
 package ru.zapovednik.db;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+/**
+ * Created by andrey-goa on 22.04.17.
+ */
+public class Tag {
+    private Integer id;
+    private String name;
 
-@Entity
-    public class Tag {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Integer id;
-
-        @NotNull
-        @Column(name = "name")
-        private String name;
-        public Integer getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setId(Integer id) {
-            this.id = id;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
+    public Integer getId() {
+        return id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
