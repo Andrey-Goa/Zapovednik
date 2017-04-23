ab508a7642d35b0de458dca80767229a1b601b96
anton.voskobovich <avoskobovitch@openwaygroup.com>
2017-04-23 08:27:56 +0300

    better tags parsing

--- src/main/java/ru/zapovednik/db/Photo.java
+++ src/main/java/ru/zapovednik/db/Photo.java
@@ -1,21 +1,25 @@
 package ru.zapovednik.db;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.util.stream.Collectors.toList;
 
 public class Photo implements Comparable<Photo> {
 
     @JsonProperty public String url;
-    @JsonProperty public List<String> tags;
+    @JsonProperty public List<String> tags = new ArrayList<>();
     @JsonProperty public float weight;
 
     public Photo(String caption, String url) {
         this.url = url;
-        this.tags = Arrays.stream(caption.split(" ")).filter(s -> s.startsWith("#")).collect(toList());
+        this.tags = Arrays.stream(StringUtils.substringAfter(caption, "#").split("#")).map(t -> StringUtils.substringBefore(t, " ")).collect(toList());
     }
 
     @Override
