ebb4618c1001b4c7f59eb19806a48bfb5eb3e46a
anton.voskobovich <avoskobovitch@openwaygroup.com>
2017-04-23 14:06:34 +0300

    added like and dislike

--- client/src/Tags.js
+++ client/src/Tags.js
@@ -6,8 +6,8 @@ export default class Tags extends React.Component {
     return (
       <div className="tags">
         {this.props.value.map(tag => <p><span onClick={() => this.props.selectTag(tag)} className={this.props.colors[tag]}>#{tag}              </span>
-        <span className="option" onClick={() => this.props.onGreenTag(tag)}>Good                   </span>
-        <span className="option" onClick={() => this.props.onRedTag(tag)}>Bad</span></p>)}
+        <span className="option" onClick={() => this.props.onGreenTag(tag)}><img src="like.png" width={20} height={20}/>                   </span>
+        <span className="option" onClick={() => this.props.onRedTag(tag)}><img src="dislike.png" width={20} height={20}/></span></p>)}
       </div>
     );
   }
