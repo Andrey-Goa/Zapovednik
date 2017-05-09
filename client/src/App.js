e48d4ab11614c40f1bc81c269bc704cb7f3b638c
anton.voskobovich <avoskobovitch@openwaygroup.com>
2017-05-09 23:41:14 +0300

    fix click on tag, support enter in search form

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     super();
     this.state = {filter: 'kransodarling', input: 'kransodarling'};
 
-    this.applyFilter = this.applyFilter.bind(this);
+    this.onFindClick = this.onFindClick.bind(this);
+    this.updateFilter = this.updateFilter.bind(this);
     this.handleFilterInputChange = this.handleFilterInputChange.bind(this);
   }
 
@@ -17,24 +18,30 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
-  applyFilter(filter) {
-    let value = (filter || this.state.input);
-    console.log(value);
-    this.setState({filter: value, input: value});
+  onFindClick(event) {
+    if(event) event.preventDefault();
+    this.updateFilter();
+  }
+
+  updateFilter(filter) {
+    this.setState({filter: filter || this.state.input, input: filter || this.state.input});
   }
 
   render() {
     return (
       <div className="App">
-        <div className="input-group">
-          <span className="input-group-addon" id="sizing-addon1">#</span>
-          <input type="text" value={this.state.input} onChange={this.handleFilterInputChange} className="form-control" aria-describedby="sizing-addon1"/>
-          <span className="input-group-btn">
-            <button className="btn btn-default" onClick={() => this.applyFilter()}>Find</button>
-           </span>
+        <form>
+          <div className="input-group">
 
-        </div>
-        <Photos filter={this.state.filter} selectTag={this.applyFilter}/>
+            <span className="input-group-addon" id="sizing-addon1">#</span>
+            <input type="text" value={this.state.input} onChange={this.handleFilterInputChange} className="form-control" aria-describedby="sizing-addon1"/>
+
+            <span className="input-group-btn">
+              <button className="btn btn-default" onClick={this.onFindClick}>Find</button>
+            </span>
+          </div>
+        </form>
+        <Photos filter={this.state.filter} selectTag={this.updateFilter}/>
       </div>
     );
 
